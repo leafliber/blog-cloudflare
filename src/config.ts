bc4db4f6bf339b339e750b8030dccbd80537f710
“Leafiber” <leaf@airo.cc>
2025-09-07 17:33:21 +0800

    update links

--- src/config.ts
+++ src/config.ts
@@ -58,21 +58,21 @@ export const profileConfig: ProfileConfig = {
 	bio: "Snatch a little leisure from a busy life",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			name: "GitHub",
+			icon: "fa6-brands:github", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
-		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
 			url: "https://github.com/Leafiber",
+		},
+		{
+			name: "Gitea",
+			icon: "pajamas:gitea",
+			url: "https://gitea.airo.cc/Fiber",
+		},
+		{
+			name: "Bilibili",
+			icon: "fa6-brands:bilibili",
+			url: "https://space.bilibili.com/11166455",
 		},
 	],
 };
