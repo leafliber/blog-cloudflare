70895bbf590a4dec078d83fb3caa79d281026a90
Fiber <leaf@airo.cc>
2025-12-25 16:02:07 +0800

    add donate page

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -15,4 +15,8 @@ export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
 		name: i18n(I18nKey.archive),
 		url: "/archive/",
 	},
+	[LinkPreset.Donate]: {
+		name: i18n(I18nKey.donate),
+		url: "/donate/",
+	},
 };
