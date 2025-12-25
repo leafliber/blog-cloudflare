5d1c9dae8fdf975349e1ae41e59fbe785f4c8355
Fiber <leaf@airo.cc>
2025-12-25 20:03:54 +0800

    new donate

---
+++ src/data/donors.ts
@@ -0,0 +1,21 @@
+export interface Donor {
+	name: string;
+	amount: string;
+	date: string;
+	message?: string;
+}
+
+export const donors: Donor[] = [
+	{
+		name: "ψ(ﾟДﾟ)ψ",
+		amount: "¥30.00",
+		date: "2025-12-25",
+		message: "献给Chito",
+	},
+	{
+		name: "白漠",
+		amount: "¥30.00",
+		date: "2025-12-25",
+		message: "献给Chito",
+	},
+];
