59455103f9a4c181758a1ebe4d90e87af57524fe
Fiber <leaf@airo.cc>
2026-05-08 16:57:10 +0800

    feat: 更新捐赠者信息，移除重复条目

--- src/data/donors.ts
+++ src/data/donors.ts
@@ -7,6 +7,12 @@ export interface Donor {
 
 export const donors: Donor[] = [
 	{
+		name: "洛临",
+		amount: "¥50.00",
+		date: "2026-05-08",
+		message: "献给Chito",
+	},
+	{
 		name: "ψ(ﾟДﾟ)ψ",
 		amount: "¥30.00",
 		date: "2025-12-25",
@@ -16,12 +22,6 @@ export const donors: Donor[] = [
 		name: "白漠",
 		amount: "¥30.00",
 		date: "2025-12-25",
-		message: "献给Chito",
-	},
-	{
-		name: "洛临",
-		amount: "¥50.00",
-		date: "2026-05-08",
 		message: "献给Chito",
 	},
 ];
